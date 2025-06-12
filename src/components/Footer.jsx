ed19c55c3830aa4bf0563e3572bbd2871e8fbee9
Samuel <srangel674@gmail.com>
2025-06-12 07:42:28 -0700

    changed homepage

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 
 const Footer = () => {
   return (
-<footer>
-  <p>© 2025 Samuel Rangel</p>
-  <div>
-    <a href="https://github.com/samwell30" target="_blank" rel="noreferrer">GitHub</a> |
-    <a href="https://linkedin.com" target="_blank" rel="noreferrer">LinkedIn</a> |
-    <a href="mailto:srangel674@gmail.com">Email</a>
-  </div>
-</footer>  )
+    <footer>
+      <p>© 2025 Samuel Rangel</p>
+      <div>
+        <a href="https://github.com/samwell30" target="_blank" rel="noreferrer">GitHub</a> |
+        <a href="https://linkedin.com" target="_blank" rel="noreferrer">LinkedIn</a> |
+        <a href="mailto:srangel674@gmail.com">Email</a>
+      </div>
+    </footer>)
 }
 
 export default Footer
