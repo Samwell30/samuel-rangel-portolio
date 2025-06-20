0569ad0f245a9da9c4cfaa2712b6e224654bece1
Samuel <srangel674@gmail.com>
2025-06-20 07:03:10 -0700

    updated about me and responsiveness to modal

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Nav from "./Ui.jsx/Nav";
 import ContactModal from "./components/ContactModal";
 import Modal from "./components/Modal";
 import Footer from "./components/Footer";
+import fire from "./assets/icons8-firebase-24.png"
 
 function App() {
   const [showContact, setShowContact] = useState(false);
@@ -16,14 +17,15 @@ function App() {
         <>
           <h2 className="modal__title">About this portfolio</h2>
           <p className="modal__para">
-            This portfolio is to display my skills and enjoyment of building React apps and solving real-world problems.
-            <br /> The projects below were created using React, Javascript, CSS, and HTML.
+            This portfolio is to display my skills and enjoyment of building React apps.
+            <br /> The projects below were created using React, Firebase Javascript, CSS, and HTML.
           </p>
           <div className="icons">
             <i class="fa-brands fa-react"></i>
             <i class="fa-brands fa-js"></i>
             <i class="fa-brands fa-html5"></i>
-            <i class="fa-brands fa-css"></i>
+            <i class="fa-brands fa-css"></i>            
+            <img src={fire} alt="Firebase icon" className="firebase-icon" />
           </div>
         </>
       );
@@ -32,8 +34,13 @@ function App() {
         <>
           <h2 className="modal__title">Hi There!</h2>
           <p className="modal__para">
-            My name is Samuel Rangel. I am excited to go on this journey to become a developer.
-            <br /> "adding short bio here...."
+            I am an aspiring developer with a passion for building and learning, driven by a deep 
+            understanding of human behavior. I have an MA in Psychology and experience as a 
+            counselor has equipped me with strong analytical, problem-solving, 
+            and communication skills, which I'm now channeling into creating impactful software 
+            solutions. I'm excited to explore opportunities where I can combine my empathy for 
+            user needs with my growing technical expertise in React, JavaScript, CSS, HTML and 
+            whatever else peaks my interest.
           </p>
         </>
       );
