fd3691e453a0634b20b29548fafd0739c180f109
Samuel Rangel <samuelr@example.com>
2025-08-22 08:53:25 -0700

    changed contact section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -14,15 +14,23 @@ const About = () => {
         <div className="about__details">
           <h2 className="about__title">About Me</h2>
           <p className="about__text">
-            Hi, I'm Samuel! I'm passionate about building beautiful, user-friendly web
-            experiences. My mission is to continuously advance in web development,
-            using my growing expertise to drive societal progress through technology.
+            Hi, I'm Samuel! Driven by a deep understanding of human behavior, I
+            am an aspiring developer with a passion for building and learning. I
+            have an MA in Psychology and experience as a counselor has equipped
+            me with strong analytical, problem-solving, and communication
+            skills, which I'm now channeling into creating impactful software
+            solutions. I'm excited to explore opportunities where I can combine
+            my empathy for user needs with my growing technical expertise.
           </p>
           <div className="techstack">
             <h3>About this portfolio</h3>
             <p className="techstack__para">
-              This portfolio is to display my skills and enjoyment of building React apps.
-              <br /> The projects below were created using React, Firebase Javascript, CSS, and HTML.
+              My mission is to continuously advance in web development, using my
+              growing expertise to drive societal progress through technology.
+              <br />
+              This portfolio is to display my skills and enjoyment of building
+              React apps. The projects below were created using React, Firebase
+              Javascript, CSS, and HTML.
             </p>
             <div className="icons">
               <i className="fa-brands fa-react tech-icon"></i>
